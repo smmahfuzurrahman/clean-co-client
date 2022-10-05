b70f7c16a064a5400457a63542ff75a5d2dca659
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-10-05 13:30:40 +0600

    Completed Navbar with dark mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
 import { Route, Routes } from "react-router-dom";
+import {publicRoute} from "./routes/publicRoutes";
 function App() {
   return (
     <>
       <Navbar>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/login' element={<Login />} />
+          {
+            publicRoute.map(({ path, Component }, index) =>
+              <Route key={index} path={path} element={<Component />} />)
+          }
         </Routes>
       </Navbar >
 
---
+++ src/routes/publicRoutes.js
@@ -0,0 +1,14 @@
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import Services from "../pages/Services";
+import Contact from "../pages/Contact";
+
+export const publicRoute = [
+    { path: "/", name: "Home", Component: Home },
+    { path: "/home", name: "Home", Component: Home },
+    { path: "/about", name: "About", Component: About },
+    { path: "/contact", name: "Contact", Component: Contact },
+    { path: "/services", name: "Services", Component: Services },
+    { path: "/login", name: "Login", Component: Login },
+]
