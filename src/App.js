09634151cdb1cd7b1377f68baee23198d1c0fce8
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-10-07 22:27:28 +0600

    Landign Page animation added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import {publicRoute} from "./routes/publicRoutes";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from "react";
 function App() {
+  useEffect(()=>{
+    AOS.init();
+  },[]);
   return (
     <>
       <Navbar>
         <Routes>
           {
             publicRoute.map(({ path, Component }, index) =>
-              <Route key={index} path={path} element={<Component />} />)
+              <Route key={index} path={path} element={<Component />} />
+              
+              )
           }
         </Routes>
       </Navbar >
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -2,17 +2,33 @@ import React from 'react';
 import BucketGirl from "../../assets/images/bucketgirl.png";
 const Landing = () => {
     return (
-        <div class="hero lg:h-[60hv] bg-accent mt-16">
-            <div class="hero-content flex-col lg:flex-row">
-                <div>
-                    <p class='text-xl'>Best Quality</p>
-                    <h1 class="text-5xl font-bold">Professional Cleaning Services</h1>
-                    <p class="py-6 max-w-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officia itaque quod iusto obcaecati error necessitatibus eos commodi cumque id?</p>
-                    <button class="btn btn-primary">Get Started</button>
+        <div class="relative">
+            <div class="hero h-screen lg:h-[60vh] bg-accent relative mt-10">
+                <div class="hero-content flex-col lg:flex-row">
+                    <div>
+                        <p data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200" class='text-xl'>Best Quality</p>
+                        <h1 data-aos="fade-right" data-aos-duration="900" data-aos-delay="400" class="text-5xl font-bold">Professional Cleaning Services</h1>
+                        <p data-aos="zoom-in" data-aos-duration="800" data-aos-delay="600"class="py-6 max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eos praesentium autem, maiores odio delectus ab neque nobis aspernatur esse, sed non consequuntur quis qui. Voluptatem sunt sed cupiditate vel.</p>
+                        <button class="btn btn-primary" data-aos="zoom-in" data-aos-delay="1000">Get Started</button>
+                    </div>
+                    <div className='lg:h-[60vh] shrink-0'>
+                        <img src={BucketGirl} class="h-full" alt='' />
+                    </div>
                 </div>
-                <div className='lg:h-[60vh] shrink-0'>
-                    <img src={BucketGirl} class="h-full" alt='/' />
+            </div>
+            <div class="rounded-2xl mx-auto mt-[-20px] relative z-20 bg-base-200 shadow-lg p-5 w-5/6">
+                <h1 class="text-2xl mb-5 text-primary">Get Free Estimate</h1>
+                <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                 </div>
+                <button class="btn btn-primary mt-5"> Get a Quote</button>
             </div>
         </div>
     );
