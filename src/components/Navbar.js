d9499606f193323ca59d9b1e2183f7b661d7f325
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-10-12 20:31:41 +0600

    Dashboard Sidebar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import useAdmin from '../hooks/useAdmin';
 
 const Navbar = ({ children }) => {
     const [dark, SetDark] = useState(false);
     const [admin] = useAdmin();
+    const { pathname } = useLocation();
     return (
         <div class="drawer drawer-end" data-theme={dark ? "dark" : "light"}>
             <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content flex flex-col">
                 <div class="w-full navbar bg-base-100 fixed top-0 lg:px-20 z-50">
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    {pathname.includes("dashboard") && (<label tabindex="0" for="my-drawer-2" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>)}
                     <div class="flex-1 px-2 mx-2 text-2xl">Clean Co.</div>
                     <div class="flex-none lg:hidden">
                         <label for="my-drawer-3" class="btn btn-square btn-ghost">
@@ -21,8 +24,8 @@ const Navbar = ({ children }) => {
                     <div class="flex-none hidden lg:block">
                         <ul class="menu menu-horizontal gap-x-2">
                             <li><NavLink to='/home' className='rounded-lg'>Home</NavLink></li>
-                            { admin && (
-                            <li><NavLink to='/dashboard' className='rounded-lg'>Dashboard</NavLink></li>
+                            {admin && (
+                                <li><NavLink to='/dashboard' className='rounded-lg'>Dashboard</NavLink></li>
                             )}
                             <li><NavLink to='/about' className='rounded-lg'>About</NavLink></li>
                             <li><NavLink to='/services' className='rounded-lg'>Services</NavLink></li>
