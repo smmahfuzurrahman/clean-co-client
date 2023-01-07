f748bfcea5255562f31f2953a4c0f30ac7f10ec3
Mahfuzur Rahman <smmahfuz787@gmail.com>
2023-01-07 21:36:09 +0600

    database connect MONGODB, install and use Axios

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import fetcher from '../api';
+
 
 const Services = () => {
+    const [services, setServices ] = useState([]);
+    console.log(services);
+    useEffect(() => {
+        //axios.get("http://localhost:5000/service").then(res => console.log(res.data));
+
+        (async () => {
+            const res = await fetcher.get("/service");
+            setServices(res.data);
+        })();
+    }, [])
     return (
         <div>
             <h1>This is Services</h1>
